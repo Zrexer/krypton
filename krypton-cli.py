03852ee750bc0b3d79696109e5ab32f9f85fa009
Zrexer <122536817+Zrexer@users.noreply.github.com>
2024-08-03 21:04:40 +0330

    Add files via upload

---
+++ krypton-cli.py
@@ -0,0 +1,10 @@
+
+from kryptoncore.atrequire import start
+from kryptoncore.otherhost import lunchMain
+from kryptoncore.beprompt import KryptonCommandLine
+
+# Check Requirements
+start()
+
+# Start Kryptons Client Side
+KryptonCommandLine()
